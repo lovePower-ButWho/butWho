77bea4caee6308e134cf94d1592d40faa584367c
chae-won-shin <scwcart@naver.com>
2024-02-28 19:58:02 +0900

    feat: zustand to useContext

--- frontend/butwho/src/pages/FirstMeetPage.jsx
+++ frontend/butwho/src/pages/FirstMeetPage.jsx
@@ -1,7 +1,8 @@
 import styled from 'styled-components';
 import {Conversation, Choice} from '../components/ScriptTexts';
 import backgroundImg from '../assets/images/firstMeet.svg';
-import {create} from 'zustand';
+import {createContext, useContext, useState} from 'react';
+import firstMeet from '../scripts/Script';
 
 const FlexContainer = styled.div`
     display: flex;
@@ -9,7 +10,7 @@ const FlexContainer = styled.div`
     justify-content: space-between;
 `;
 
-const Pages = styled.div`
+const PagesStyle = styled.div`
     width: 360px;
     height: 800px;
     background-image: url(${(props) => props.image});
@@ -18,40 +19,47 @@ const Pages = styled.div`
     position: relative;
 `;
 
-const firstMeet = [{
-    type:'narration',
-    text:'설레는 신학기! 사진 동아리에 새로 가입했다'
-}, {
-    type: 'narration',
-    text: '음... 동아리 첫 모임인데 아무도 말을 하지 않네...'
-}, {
-    type: 'choice',
-    text: ['조용히 기다린다', '누군가에게 말을 건넨다']
-}];
+const NarrationIndex = createContext(null);
+
+const Page = ()=> {
+    const { index, increaseIndex } = useContext(NarrationIndex);
+
+    return (
+        <PagesStyle onClick={increaseIndex} image={backgroundImg}>
+            {firstMeet[index].type === 'narration' && <Conversation name='나' image={gojyoImage} text={firstMeet[index].text} />}
+            {firstMeet[index].type === 'choice' && <Choice text={firstMeet[index].text} />}
+        </PagesStyle>
+    );
+}
 
 const gojyoImage = "https://i.namu.wiki/i/ZUrEBCVWrZwEIQ6KjWWNvsfSGvVgntvCWGIpdZmrtZaWjG3pBaF16sesKxsU2LtQfJFqtHaoXyGSoB2Kbj2OFQ.webp";
 
-const narrationIndex = create((set)=>({
-    index : 0,
-    setIndex(){
-        set((state)=> {
-            if(state.index === firstMeet.length-1) {
-                return state;
-            }
-            return {index: state.index + 1};
-    });
-  },
-}));
+// const narrationIndex = create((set)=>({
+//     index : 0,
+//     setIndex(){
+//         set((state)=> {
+//             if(state.index === firstMeet.length-1) {
+//                 return state;
+//             }
+//             return {index: state.index + 1};
+//     });
+//   },
+// }));
 
 function FirstMeetPage(){
-    const {index, setIndex} = narrationIndex();
+    // const {index, setIndex} = narrationIndex();
+
+    const [index, setIndex] = useState(0);
+    const increaseIndex = ()=>{
+        if(index === firstMeet.length-1) return;
+        setIndex(index+1)
+    };
 
     return(
         <FlexContainer>
-            <Pages onClick={setIndex} image={backgroundImg}>
-                {firstMeet[index].type === 'narration' && <Conversation name='나' image={gojyoImage} text={firstMeet[index].text} />}
-                {firstMeet[index].type === 'choice' && <Choice text={firstMeet[index].text} />}
-            </Pages>
+            <NarrationIndex.Provider value={{index, increaseIndex}}>
+                <Page/>
+            </NarrationIndex.Provider>
         </FlexContainer>
     )
 }
--- frontend/butwho/src/scripts/Script.jsx
+++ frontend/butwho/src/scripts/Script.jsx
@@ -1,4 +1,12 @@
-const firstMeet
-= ['설레는 신학기!', '두근두근'];
+const firstMeet = [{
+    type:'narration',
+    text:'설레는 신학기! 사진 동아리에 새로 가입했다'
+}, {
+    type: 'narration',
+    text: '음... 동아리 첫 모임인데 아무도 말을 하지 않네...'
+}, {
+    type: 'choice',
+    text: ['조용히 기다린다', '누군가에게 말을 건넨다']
+}];
 
 export default firstMeet;
